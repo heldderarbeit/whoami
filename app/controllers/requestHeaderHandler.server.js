ec195aebfaa0bdce3337518ae132330473720cba
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-11-24 13:07:17 +0100

    Update requestHeaderHandler.server.js

--- app/controllers/requestHeaderHandler.server.js
+++ app/controllers/requestHeaderHandler.server.js
@@ -6,13 +6,13 @@ function requestHeaderHandler () {
 	    
 	    // look for the originating IP address in the X-Forwarded-For HTTP header, 
 	    // see https://en.wikipedia.org/wiki/X-Forwarded-For
-        var IPAddress = req.header('x-forwarded-for') || req.connection.remoteAdress;
+            var IPAddress = req.header('x-forwarded-for') || req.connection.remoteAdress;
         
-        var language = req.headers['accept-language'].split(';')[0].split(',')[0];
+            var language = req.headers['accept-language'].split(';')[0].split(',')[0];
         
-        var os = req.headers['user-agent'].split('(')[1].split(')')[0];
+            var os = req.headers['user-agent'].split('(')[1].split(')')[0];
         
-        res.send({ 'IP Address':  IPAddress, 'language': language, 'operating system': os });
+            res.send({ 'IP Address':  IPAddress, 'language': language, 'operating system': os });
 	};
 }
 
